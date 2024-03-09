42ff4cc1d5b255e9a13db1b2400df051f55901c0
lc-fei <3251801949@qq.com>
2024-03-09 20:39:10 +0800

    项目初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,75 @@
+<template>
+	<view class="content">
+		<view hover-class="hhh" class="www">我是view</view>
+		<text selectable="true ">dwadawdawd</text>
+		<scroll-view scroll-y="true" class="jdkl">
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<text>djsalkas</text>
+			<view class="">
+				dsadsadasda
+			</view><view class="">
+				dsadsadasda
+			</view><view class="">
+				dsadsadasda
+			</view><view class="">
+				dsadsadasda
+			</view><view class="">
+				dsadsadasda
+			</view><view class="">
+				dsadsadasda
+			</view><view class="">
+				dsadsadasda
+			</view><view class="">
+				dsadsadasda
+			</view>
+		</scroll-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.www {
+		height: 100px;
+		width: 200px;
+	}
+	.hhh {
+		background-color: pink;
+	}
+	.jdkl {
+		height: 100px;
+	}
+</style>
