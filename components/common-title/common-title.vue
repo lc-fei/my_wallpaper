ddf343af1c1492e45dd1979f3cc60dc1e38f1d0b
lc-fei <3251801949@qq.com>
2024-03-12 13:35:41 +0800

    完成每日推荐样式

---
+++ components/common-title/common-title.vue
@@ -0,0 +1,36 @@
+<template>
+	<view class="common-title">
+		<view class="name">
+			<slot name="name"></slot>
+		</view>
+		<view class="custom">
+			<slot name="custom"></slot>
+		</view>
+	</view>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+	.common-title {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		flex-wrap: nowrap;
+		padding: 0 30rpx;
+		align-items: center;
+
+		.name {
+			font-size: 40rpx;
+		}
+
+		.custom {
+			display: flex;
+			flex-direction: row;
+			flex-wrap: nowrap;
+			align-items: center;
+			font-size: 25rpx;
+		}
+	}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,11 +1,17 @@
 <template>
 	<view class="home-layout">
 		<swiper class="banner" indicator-dots="true" active-class="swiper-active">
-			<swiper-item><image src="@/common/images/banner1.jpg" mode="aspectFill"/></swiper-item>
-			<swiper-item><image src="@/common/images/banner2.jpg" mode="aspectFill"/></swiper-item>
-			<swiper-item><image src="@/common/images/banner3.jpg" mode="aspectFill"/></swiper-item>
+			<swiper-item>
+				<image src="@/common/images/banner1.jpg" mode="aspectFill" />
+			</swiper-item>
+			<swiper-item>
+				<image src="@/common/images/banner2.jpg" mode="aspectFill" />
+			</swiper-item>
+			<swiper-item>
+				<image src="@/common/images/banner3.jpg" mode="aspectFill" />
+			</swiper-item>
 		</swiper>
-		
+
 		<view class="notice">
 			<view class="left">
 				<uni-icons type="sound-filled" size="20" color="#2AB18D"></uni-icons>
@@ -13,34 +19,65 @@
 			</view>
 			<view class="center">
 				<swiper autoplay interval="2000" duration="300" vertical="true">
-					<swiper-item v-for="item in 4">欢迎来到柚见壁纸djwladdjasldjaksldjlawdjaqwjdaksljdoasdujiaos{{item}}</swiper-item>
+					<swiper-item
+						v-for="item in 4">欢迎来到柚见壁纸djwladdjasldjaksldjlawdjaqwjdaksljdoasdujiaos{{item}}</swiper-item>
 				</swiper>
 			</view>
 			<view class="right">
 				<uni-icons type="forward" size="20" color="#A7A7A7"></uni-icons>
 			</view>
+		</view>
 
-			
+		<view class="select">
+			<view class="select-title">
+				<common-title>
+					<template #name>每日推荐</template>
+					<template #custom>
+						<view class="icon">
+							<uni-icons type="calendar" size="30rpx" color="#2AB18D"></uni-icons>
+						</view>
+						<view class="data">
+							<uni-dateformat :date="new Date()" format="dd日"></uni-dateformat>
+						</view>
+					</template>
+				</common-title>
+			</view>
+			<view class="body">
+				<scroll-view scroll-x show-scrollbar="true">
+					<view v-for="item in 6">
+						<image src="../../common/images/preview_small.webp" mode="aspectFill"></image>
+					</view>
+				</scroll-view>
+			</view>
+		</view>
+
+		<view class="theme">
+			<common-title>
+				<template #name>专题精选</template>
+				<template #custom>More+</template>
+			</common-title>
 		</view>
 	</view>
 </template>
 
 <script setup lang="ts">
-// import { onMounted, ref } from 'vue';
+	// import { onMounted, ref } from 'vue';
 // import {onReachBottom} from '@dcloudio/uni-app'
 </script>
 
 <style lang="scss">
 	.home-layout {
 		width: 750rpx;
-		height: 345rpx;
+
 		.banner {
-			width: 750rpx;
 			height: 345rpx;
+			width: 750rpx;
 			overflow: hidden;
+
 			swiper-item {
 				border-radius: 20rpx;
 				padding: 0 30rpx;
+
 				image {
 					height: 100%;
 					width: 100%;
@@ -48,7 +85,7 @@
 			}
 		}
 	}
-	
+
 	.notice {
 		width: 690rpx;
 		height: 80rpx;
@@ -58,36 +95,70 @@
 		border-radius: 80rpx;
 		margin: 0 auto;
 		margin-top: 30rpx;
+
 		.left {
 			width: 140rpx;
 			display: flex;
 			align-items: center;
-			justify-content: center;  
-			}
+			justify-content: center;
 		}
-		.center  {
+
+		.center {
 			flex: 1;
+
 			swiper {
 				width: 100%;
 				height: 100%;
 				line-height: 80rpx;
-			    &-item {
+
+				&-item {
 					overflow: hidden;
 					white-space: nowrap;
 					text-overflow: ellipsis;
 				}
-				
+
 			}
 		}
-		.right{
+
+		.right {
 			width: 70rpx;
 			display: flex;
 			align-items: center;
-			justify-content: center;  
+			justify-content: center;
 		}
-		
-	
-	
+	}
+
+
+	.select {
+		scroll-view {
+			white-space: nowrap;
+
+		}
+
+		.body {
+			width: 720rpx;
+			margin-left: 30rpx;
+
+			view {
+				margin-right: 15rpx;
+				height: 446rpx;
+				width: 202rpx;
+				display: inline-block;
+
+				image {
+					height: 100%;
+					width: 100%;
+				}
+			}
+
+			view:last-child {
+				margin-right: 30rpx;
+			}
+		}
+
+
+	}
+
 	swiper-active {
 		background-color: red;
 	}
