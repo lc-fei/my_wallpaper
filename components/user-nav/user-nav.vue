8bf77baea2d39f0a40d69e0d94e92935bce233e2
lc-fei <3251801949@qq.com>
2024-03-14 14:05:00 +0800

    完成我的样式

---
+++ components/user-nav/user-nav.vue
@@ -0,0 +1,63 @@
+<template>
+	<view class="user-nav">
+		<navigator :url="url">
+			<view class="left">
+				<uni-icons :type="iconName" size="25" style="margin-right: 10rpx;"></uni-icons>
+				<view class="text">{{text}}</view>
+			</view>
+			<view class="right">
+				<view v-if="isNumber" style="font-size: 30rpx;">{{number}}</view>
+				<view><uni-icons type="right" size="20" color="#666"></uni-icons></view>
+			</view>
+		</navigator>
+	</view>
+</template>
+
+<script setup lang="ts">
+	defineProps({
+		url: {
+			type: String,
+		},
+		iconName: {
+			type: String
+		},
+		text: {
+			type: String
+		},
+		isNumber: {
+			type: Boolean,
+			default: false
+		},
+		number: {
+			type: Number
+		}
+	})
+</script>
+
+<style scoped lang="scss">
+	.user-nav {
+		width: 690rpx;
+
+		navigator {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			height: 100rpx;
+			padding: 0 30rpx;
+			border-bottom: 1px solid #eee;
+
+			.right,
+			.left {
+				display: flex;
+				align-items: center;
+				justify-content: center;
+			}
+
+			.right {
+				view {
+					color: #666;
+				}
+			}
+		}
+	}
+</style>
--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,22 +1,81 @@
 <template>
-	<view>
-		我是用户页面
+	<view class="userLayout">
+		<view class="head">
+			<view class="avatar">
+				<image src="../../static/images/xxmLogo.png" mode="aspectFill"></image>
+			</view>
+			<view class="ip">
+				100.100.100.100
+			</view>
+			<view class="from">来自于: 山东</view>
+		</view>
+
+		<view class="list-1">
+			<user-nav url="/pages/classlist/classlist" iconName="download-filled" text="我的下载" isNumber="true"
+				number="1">
+			</user-nav><user-nav url="/pages/classlist/classlist" iconName="star-filled" text="我的评分" isNumber="true"
+				number="1">
+			</user-nav><user-nav url="/pages/classlist/classlist" iconName="chatboxes-filled" text="联系客服"></user-nav>
+		</view>
+		<view class="list-2">
+			<user-nav url="/pages/classlist/classlist" iconName="notification-filled" text="订阅更新"></user-nav>
+			<user-nav url="/pages/classlist/classlist" iconName="flag-filled" text="常见问题"></user-nav>
+		</view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-
-			}
-		},
-		methods: {
-
-		}
-	}
+<script setup lang="ts">
 </script>
 
-<style>
+<style lang="scss">
+	.head {
+		height: 300rpx;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		flex-direction: column;
 
+		.avatar {
+			height: 160rpx;
+			width: 160rpx;
+			overflow: hidden;
+			border-radius: 80rpx;
+
+			image {
+				height: 100%;
+				width: 100%;
+			}
+		}
+
+		.ip {
+			margin: 38rpx 0 24rpx 0;
+			font-size: 44rpx;
+			color: #333;
+		}
+
+		.from {
+			font-size: 28rpx;
+			color: #aaa;
+		}
+
+	}
+
+	.list-1,
+	.list-2 {
+		width: 690rpx;
+		margin: 0 30rpx;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		border: 1px solid #eee;
+	}
+
+	.list-1 {
+		margin-top: 100rpx;
+	}
+
+	.list-2 {
+		margin-top: 50rpx;
+	}
 </style>
